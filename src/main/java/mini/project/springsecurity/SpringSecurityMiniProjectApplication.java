be584989a9b6b0b2f50be396fd860d7d14369590
minhyeok <omh34658981@gmail.com>
2023-05-29 19:42:29 +0900

    initial setup

---
+++ src/main/java/mini/project/springsecurity/SpringSecurityMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package mini.project.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMiniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mini/project/springsecurity/SpringSecurityMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.project.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
